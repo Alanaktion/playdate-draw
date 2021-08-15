e7fb8341dc30aeff4469866f6cd90c12cb8b8087
Alan Hardman <alanaktion@gmail.com>
2021-08-15 00:41:28 -0600

    Switch to id-based layers

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -2,13 +2,13 @@
   <div class="playdate">
     <div class="playdate-bezel">
       <div class="canvas-container" @mousemove="point" @mouseleave="point">
-        <template v-for="(layer, i) in layers" :key="i">
+        <template v-for="layer in layers" :key="layer.id">
           <canvas
-            :ref="el => setCanvasRef(el, i)"
+            :ref="el => setCanvasRef(el, layer.id)"
             :width="width"
             :height="height"
             :style="{
-              zIndex: i,
+              zIndex: layer.index,
             }"
           />
         </template>
@@ -18,20 +18,20 @@
   <div class="layers">
     <ol>
       <li
-        v-for="(layer, i) in layers"
-        :key="i"
+        v-for="layer in sortedLayers"
+        :key="layer.id"
         class="flex items-center"
-        @click="currentLayer = i"
+        @click="currentLayer = layer.id"
       >
         <canvas
-          :ref="el => setLayerCanvasRef(el, i)"
+          :ref="el => setLayerCanvasRef(el, layer.id)"
           :width="width"
           :height="height"
           class="layer-preview"
         />
         <div>
-          Layer {{ i + 1 }}
-          <span class="current" v-if="i === currentLayer">
+          {{ layer.name }}
+          <span class="current" v-if="layer.id === currentLayer">
             (current)
           </span>
         </div>
@@ -42,85 +42,106 @@
 </template>
 
 <script>
+import { sortBy } from 'lodash'
+
 const width = 400
 const height = 240
 
 const white = '#b1afa8'
 const black = '#312f28'
 
+let lastId = 0
+
 export default {
   name: 'Draw',
   emits: ['move'],
   data: () => ({
     currentLayer: null,
-    layers: [],
+    layers: {},
     canvases: {},
     layerCanvases: {},
     width,
     height,
   }),
+  computed: {
+    sortedLayers() {
+      return sortBy(this.layers, layer => layer.index)
+    },
+  },
   methods: {
-    setCanvasRef(el, i) {
+    setCanvasRef(el, id) {
       if (el) {
-        this.canvases[i] = el
+        this.canvases[id] = el
       }
     },
-    setLayerCanvasRef(el, i) {
+    setLayerCanvasRef(el, id) {
       if (el) {
-        this.layerCanvases[i] = el
+        this.layerCanvases[id] = el
       }
     },
-    render(layer) {
-      const canvas = this.canvases[layer]
+    render(id) {
+      // Render full size canvas
+      const canvas = this.canvases[id]
       if (!canvas) {
-        console.warn(`Can't find canvas for layer ${layer}`)
+        console.warn(`Can't find canvas for layer ${id}`)
         return
       }
+
+      const { data } = this.layers[id]
+
       const ctx = canvas.getContext('2d')
       ctx.clearRect(0, 0, canvas.width, canvas.height)
-      if (this.layers[layer]) {
-        ctx.putImageData(this.layers[layer], 0, 0)
+      if (data) {
+        ctx.putImageData(data, 0, 0)
       }
 
-      const layerCanvas = this.layerCanvases[layer]
+      // Render preview image
+      const layerCanvas = this.layerCanvases[id]
       if (!layerCanvas) {
         return
       }
       const ctx2 = layerCanvas.getContext('2d')
       ctx2.clearRect(0, 0, layerCanvas.width, layerCanvas.height)
-      if (this.layers[layer]) {
-        ctx2.putImageData(this.layers[layer], 0, 0)
+      if (data) {
+        ctx2.putImageData(data, 0, 0)
       }
     },
     newLayer() {
       const data = new ImageData(width, height)
-      const i = this.layers.push(data)
-      this.currentLayer = i - 1
-      return i - 1
+      lastId += 1
+      this.layers[lastId] = {
+        id: lastId,
+        index: Object.keys(this.layers).length,
+        name: `Layer ${lastId}`,
+        visible: true,
+        data,
+      }
+      this.currentLayer = lastId
+      return lastId
     },
-    clear(layer) {
+    clear(id) {
       const data = new ImageData(width, height)
-      return this.layers[layer] = data
+      return this.layers[id].data = data
     },
-    draw(layer, callback) {
+    draw(id, callback) {
       const canvas = document.createElement("canvas")
       canvas.width = width
       canvas.height = height
       const ctx = canvas.getContext('2d')
-      if (this.layers[layer]) {
-        ctx.putImageData(this.layers[layer], 0, 0)
+      if (this.layers[id].data) {
+        ctx.putImageData(this.layers[id].data, 0, 0)
       }
       callback(ctx)
-      this.layers[layer] = ctx.getImageData(0, 0, width, height)
+      this.layers[id].data = ctx.getImageData(0, 0, width, height)
     },
-    rect(layer, x, y, w, h, color) {
-      this.draw(layer, ctx => {
+    rect(id, x, y, w, h, color) {
+      this.draw(id, ctx => {
         ctx.fillStyle = color
         ctx.fillRect(x, y, w, h)
       })
     },
-    outlineRect(layer, x, y, w, h, color, lineWidth=1) {
-      this.draw(layer, ctx => {
+    outlineRect(id, x, y, w, h, color, lineWidth=1) {
+      this.draw(id, ctx => {
         ctx.strokeStyle = color
         ctx.lineWidth = lineWidth
         ctx.strokeRect(x, y, w, h)
@@ -129,7 +150,7 @@ export default {
     point(e) {
       if (e.type === 'mousemove') {
         this.$emit('move', e.offsetX, e.offsetY)
-        if (e.which === 1) {
+        if (e.buttons === 1) {
           this.rect(this.currentLayer, e.offsetX, e.offsetY, 1, 1, white)
         }
       } else {
@@ -140,22 +161,23 @@ export default {
   watch: {
     layers: {
       handler(newVal) {
-        for (let i = 0; i < newVal.length; i++) {
-          this.render(i)
-        }
+        Object.keys(newVal).forEach(id => {
+          this.render(id)
+        })
       },
       deep: true,
     },
   },
   beforeUpdate() {
-    this.canvases = []
+    this.canvases = {}
+    this.layerCanvases = {}
   },
   mounted() {
-    const i = this.newLayer()
+    const id = this.newLayer()
     this.$nextTick(() => {
-      this.rect(i, 10, 10, 20, 20, white)
-      this.rect(i, 20, 20, 20, 20, black)
-      this.outlineRect(i, 30.5, 30.5, 20, 20, white, 1)
+      this.rect(id, 10, 10, 20, 20, white)
+      this.rect(id, 20, 20, 20, 20, black)
+      this.outlineRect(id, 30.5, 30.5, 20, 20, white, 1)
     })
   },
 }
@@ -196,15 +218,16 @@ export default {
   margin: 0;
   padding: 0;
 }
+.layers li {
+  margin-bottom: 2px;
+}
 .layer-preview {
   width: 60px;
   height: 36px;
-  margin-top: 0.25rem;
-  margin-bottom: 0.25rem;
   margin-right: 1rem;
   background-color: var(--screen-black);
 }
-.current {
+.layers .current {
   color: var(--yellow);
 }
 </style>
